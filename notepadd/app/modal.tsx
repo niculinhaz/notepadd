01be53ff21f1eddfc646da8e3647e53dbcc6e47d
niculinhaz <nicholasfp@hotmail.com>
2025-12-02 10:28:23 -0300

    feat: implementação básica das rotas + mock de uma lista

--- notepadd/app/modal.tsx
+++ notepadd/app/modal.tsx
@@ -1,18 +1,12 @@
 import { Link } from 'expo-router';
 import { StyleSheet } from 'react-native';
 
-import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
 import React from 'react';
 
 export default function ModalScreen() {
   return (
-    <ThemedView style={styles.container}>
-      <ThemedText type="title">This is a modal</ThemedText>
-      <Link href="/" dismissTo style={styles.link}>
-        <ThemedText type="link">Go to home screen</ThemedText>
-      </Link>
-    </ThemedView>
+    <>
+    </>
   );
 }
 
